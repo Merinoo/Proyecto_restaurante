185f79964dc1fba0d9d2cb24af92c9d1435103fb
“Merino” <amerino96@gmail.com>
2016-02-27 20:25:11 +0100

    Proyecto/

--- Proyecto/SQL Proyecto/proyecto.sql
+++ Proyecto/SQL Proyecto/proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-02-2016 a las 19:58:50
--- Versión del servidor: 5.6.24
--- Versión de PHP: 5.5.24
+-- Tiempo de generación: 27-02-2016 a las 14:15:31
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `proyecto`
@@ -43,7 +43,27 @@ CREATE TABLE IF NOT EXISTS `detalle_pedido` (
   `codlinea` int(11) NOT NULL,
   `Pedidos_Num_pedido` int(11) NOT NULL,
   `Producto_IdProducto` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `detalle_pedido`
+--
+
+INSERT INTO `detalle_pedido` (`Cantidad`, `codlinea`, `Pedidos_Num_pedido`, `Producto_IdProducto`) VALUES
+(3, 40, 36, 14),
+(2, 41, 37, 15),
+(2, 42, 38, 13),
+(2, 43, 38, 14),
+(3, 44, 39, 14),
+(1, 45, 39, 15),
+(2, 46, 40, 14),
+(2, 47, 40, 15),
+(1, 48, 41, 13),
+(2, 49, 41, 14),
+(2, 50, 41, 15),
+(2, 51, 42, 14),
+(3, 52, 43, 14),
+(2, 53, 43, 15);
 
 -- --------------------------------------------------------
 
@@ -56,7 +76,21 @@ CREATE TABLE IF NOT EXISTS `pedidos` (
   `Usuario_idusuario` int(11) NOT NULL,
   `Fecha_pedido` date DEFAULT NULL,
   `Coste_total` decimal(9,2) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`Num_pedido`, `Usuario_idusuario`, `Fecha_pedido`, `Coste_total`) VALUES
+(36, 3, '0000-00-00', '4.40'),
+(37, 3, '0000-00-00', '4.00'),
+(38, 3, '2016-02-27', '9.40'),
+(39, 3, '2016-02-27', '7.40'),
+(40, 3, '2016-02-27', '7.40'),
+(41, 3, '2016-02-27', '10.80'),
+(42, 3, '2016-02-27', '2.80'),
+(43, 6, '2016-02-27', '8.20');
 
 -- --------------------------------------------------------
 
@@ -71,29 +105,16 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `Precio` decimal(6,2) DEFAULT NULL,
   `Cantidad` int(11) DEFAULT NULL,
   `Imagen` varchar(300) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`IdProducto`, `Tipo_producto`, `Nombre`, `Precio`, `Cantidad`, `Imagen`) VALUES
-(16, 'Bebidas', 'Coca cola', '1.20', 100, 'cocacola.jpg'),
-(17, 'Bebidas', 'Fanta', '1.20', 100, 'fanta.jpg'),
-(18, 'Bebidas', 'Nestea', '1.00', 100, 'nestea.jpg'),
-(19, 'Bebidas', 'Pepsi', '1.00', 100, 'pepsi.jpg'),
-(20, 'Bebidas', 'Aquarius', '1.50', 100, 'aquarius.jpg'),
-(21, 'Comida', 'Hamburguesa', '2.50', 100, 'Hamburguesa.jpg'),
-(22, 'Comida', 'Pizza', '3.50', 100, 'Pizza.jpeg'),
-(23, 'Comida', 'Sandwich', '2.00', 100, 'Sandwich.jpg'),
-(24, 'Comida', 'Serranito', '3.50', 100, 'Serranito.jpg'),
-(25, 'Comida', 'Baguette-pollo', '2.70', 100, 'Baguette_pollo.jpg'),
-(26, 'Postres', 'Magnum-almendrado', '2.00', 100, 'magnum-almendrado.png'),
-(27, 'Postres', 'Magnum-Frac', '2.00', 100, 'magnum-frac.png'),
-(28, 'Postres', 'Magnum-Blanco', '2.00', 100, 'magnum-blanco.png'),
-(29, 'Postres', 'Magnum-pink', '2.50', 100, 'magnum-pink.png'),
-(30, 'Postres', 'Magnum-Doble-Chocolate', '2.50', 100, 'magnum-double-chocolate.png'),
-(31, 'Complementos', 'Ensalada del chef', '3.50', 50, 'ensalada del chef.jpg');
+(13, 'Comida', 'hg', '4.00', 4, 'fz___lancer_by_janemere-d4ibhbh.png.jpg'),
+(14, 'Bebida', 'fanta', '1.40', 10, '542272_379562578747521_507750072_n.jpg'),
+(15, 'Comida', 'hamburguesa', '2.00', 10, 'comidas.jpg');
 
 -- --------------------------------------------------------
 
@@ -116,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `Sexo` varchar(6) DEFAULT NULL,
   `FNacimiento` date DEFAULT NULL,
   `Direccion` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -124,7 +145,8 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 
 INSERT INTO `usuarios` (`idusuario`, `Username`, `Password`, `Email`, `Actividad`, `Tipo`, `Dni_usuario`, `Nombre`, `Apellidos`, `Cpostal`, `Telefono`, `Sexo`, `FNacimiento`, `Direccion`) VALUES
 (3, 'japon', '827ccb0eea8a706c4c34a16891f84e7b', 'juanantoniojapon@gmail.com', 'Activo', 'user', '5741852B', 'Juan Antonio', 'Japon', 41896, 603746949, 'Hombre', '2016-02-02', 'C/San Vicente de Paul'),
-(4, 'merino', '81dc9bdb52d04dc20036dbd8313ed055', 'amerino96@gmail.com', 'Activo', 'admin', '53344470H', 'Antonio Manuel', 'Merino Soto', 41900, 679210535, 'Hombre', '0000-00-00', 'C/Argantonio Nº6');
+(4, 'merino', '81dc9bdb52d04dc20036dbd8313ed055', 'amerino96@gmail.com', 'Activo', 'admin', '53344470H', 'Antonio Manuel', 'Merino Soto', 41900, 679210535, 'Hombre', '0000-00-00', 'C/Argantonio Nº6'),
+(6, 'b', '81dc9bdb52d04dc20036dbd8313ed055', 'b@gmail.com', 'Activo', 'user', '8767678d', 'b', 'b', 33333, 444444444, 'Hombre', '0000-00-00', 'b');
 
 --
 -- Índices para tablas volcadas
@@ -134,19 +156,23 @@ INSERT INTO `usuarios` (`idusuario`, `Username`, `Password`, `Email`, `Actividad
 -- Indices de la tabla `cesta`
 --
 ALTER TABLE `cesta`
-  ADD PRIMARY KEY (`Usuarios_idusuario`,`Producto_IdProducto`), ADD KEY `fk_Cesta_Producto1_idx` (`Producto_IdProducto`);
+  ADD PRIMARY KEY (`Usuarios_idusuario`,`Producto_IdProducto`),
+  ADD KEY `fk_Cesta_Producto1_idx` (`Producto_IdProducto`);
 
 --
 -- Indices de la tabla `detalle_pedido`
 --
 ALTER TABLE `detalle_pedido`
-  ADD PRIMARY KEY (`codlinea`,`Pedidos_Num_pedido`,`Producto_IdProducto`), ADD KEY `fk_Detalle pedido_Producto1_idx` (`Producto_IdProducto`), ADD KEY `fk_Detalle pedido_Pedidos1` (`Pedidos_Num_pedido`);
+  ADD PRIMARY KEY (`codlinea`,`Pedidos_Num_pedido`,`Producto_IdProducto`),
+  ADD KEY `fk_Detalle pedido_Producto1_idx` (`Producto_IdProducto`),
+  ADD KEY `fk_Detalle pedido_Pedidos1` (`Pedidos_Num_pedido`);
 
 --
 -- Indices de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`Num_pedido`,`Usuario_idusuario`), ADD KEY `fk_Pedidos_Usuario_idx` (`Usuario_idusuario`);
+  ADD PRIMARY KEY (`Num_pedido`,`Usuario_idusuario`),
+  ADD KEY `fk_Pedidos_Usuario_idx` (`Usuario_idusuario`);
 
 --
 -- Indices de la tabla `producto`
@@ -158,7 +184,9 @@ ALTER TABLE `producto`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`idusuario`), ADD UNIQUE KEY `Dni_usuario_UNIQUE` (`Dni_usuario`), ADD UNIQUE KEY `Username_UNIQUE` (`Username`);
+  ADD PRIMARY KEY (`idusuario`),
+  ADD UNIQUE KEY `Dni_usuario_UNIQUE` (`Dni_usuario`),
+  ADD UNIQUE KEY `Username_UNIQUE` (`Username`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -168,22 +196,22 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `detalle_pedido`
 --
 ALTER TABLE `detalle_pedido`
-  MODIFY `codlinea` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=58;
+  MODIFY `codlinea` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=54;
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `Num_pedido` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
+  MODIFY `Num_pedido` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=44;
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `IdProducto` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+  MODIFY `IdProducto` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Restricciones para tablas volcadas
 --
@@ -192,21 +220,21 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `cesta`
 --
 ALTER TABLE `cesta`
-ADD CONSTRAINT `fk_Cesta_Producto1` FOREIGN KEY (`Producto_IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Cesta_Usuarios1` FOREIGN KEY (`Usuarios_idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Cesta_Producto1` FOREIGN KEY (`Producto_IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Cesta_Usuarios1` FOREIGN KEY (`Usuarios_idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `detalle_pedido`
 --
 ALTER TABLE `detalle_pedido`
-ADD CONSTRAINT `fk_Detalle pedido_Pedidos1` FOREIGN KEY (`Pedidos_Num_pedido`) REFERENCES `pedidos` (`Num_pedido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Detalle pedido_Producto1` FOREIGN KEY (`Producto_IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Detalle pedido_Pedidos1` FOREIGN KEY (`Pedidos_Num_pedido`) REFERENCES `pedidos` (`Num_pedido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Detalle pedido_Producto1` FOREIGN KEY (`Producto_IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-ADD CONSTRAINT `fk_Pedidos_Usuario` FOREIGN KEY (`Usuario_idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Pedidos_Usuario` FOREIGN KEY (`Usuario_idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
